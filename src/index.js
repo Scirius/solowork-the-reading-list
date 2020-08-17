9fbdfa10c6d4149d8c280df9183aa88e7534490e
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-17 11:44:48 +0200

    added date comparison to get lastBookRead, updated examples

--- src/index.js
+++ src/index.js
@@ -18,12 +18,23 @@ class BookList {
     this._lastBookRead = null;
   }
 
-  get NumberOfReadBooks() {
+  get numberOfReadBooks() {
     return this._numberOfReadBooks;
+  }
+  get currentBook() {
+    return `${this._currentBook._title} by ${this._currentBook._author}`;
+  }
+  get lastBookRead() {
+    if (this._lastBookRead) {
+      return `${this._lastBookRead._title} by ${this._lastBookRead._author} on ${this._lastBookRead._dateRead}`;
+    } else {
+      return "There is no read book in the list.";
+    }
   }
 
   /**
    * Adds a new book to the list and counts read/unread
+   * also checks which book was read last (read==true and latest _dateRead)
    *
    * @param {Book} book The book to add.
    */
@@ -31,23 +42,36 @@ class BookList {
     this._books.push(book);
     if (book.read) {
       this._numberOfReadBooks++;
+      if (this._lastBookRead) {
+        if (this._lastBookRead._dateRead < book._dateRead) {
+          this._lastBookRead = book;
+        }
+      } else {
+        this._lastBookRead = book;
+      }
     } else {
       this._numberOfUnreadBooks++;
     }
   }
   /**
-   *  Actually starts reading
+   * Gets first unread book from list
+   *
+   */
+  getFirstUnreadBook() {
+    return this._books.find((book) => book.read === false);
+  }
+  /**
+   *  Actually starts reading with the first unread on list
    */
   startReading() {
-    if (this._books.length == 0) {
+    if (this._books.length === 0) {
       console.log("You have no books in your list to read.");
       return;
     } else {
-      var newBook = this._books.find((book) => book.read === false);
+      var newBook = this.getFirstUnreadBook();
       if (newBook) {
-        console.log(
-          "Ok, you start reading with your first unread book on list. {newBook._title}"
-        );
+        console.log(`Ok, you start reading with your first unread book on list (${newBook._title} by ${newBook._author})`);
+        this._currentBook = newBook;
       } else {
         console.log("Oh no, you have no more unread books!");
       }
@@ -66,8 +90,9 @@ class BookList {
     if (!this._currentBook) {
       console.log("There is no book you're currently reading.");
     } else {
-      this._currentBook.read = true;
-      this._currentBook.dateRead = new Date(Date.now());
+      console.log(`You just finished reading ${this._currentBook._title} by ${this._currentBook._author}`);
+      this._currentBook._read = true;
+      this._currentBook._dateRead = new Date(Date.now());
       this._lastBookRead = this._currentBook;
     }
 
@@ -89,12 +114,12 @@ class Book {
    * @param {boolean} read The status of the book being read
    * @param {date} dateRead The date of the book being read
    */
-  constructor(title, genre, author, read = false) {
+  constructor(title, genre, author, read = false, dateRead = null) {
     this._title = title;
     this._genre = genre;
     this._author = author;
     this._read = read;
-    this._dateRead = null; //finished
+    this._dateRead = dateRead; //finished
   }
   /**
    * returns read status via getter
@@ -124,14 +149,27 @@ class Book {
  */
 const bookList = new BookList();
 
-bookList.add(
-  new Book("The Mambo Kings Play Songs of Love", "novel", "Oscar Hijuelos")
-);
-bookList.add(new Book("Foundation", "Science-fiction", "Isaac Asimov", true));
-bookList.add(new Book("Endgame", "Play", "Samuel Beckett", true));
+bookList.add(new Book("The Mambo Kings Play Songs of Love", "novel", "Oscar Hijuelos"));
+bookList.add(new Book("Foundation", "Science-fiction", "Isaac Asimov", true, new Date("2020-08-01T23:24:00")));
+bookList.add(new Book("Endgame", "Play", "Samuel Beckett", true, new Date("2020-08-07T19:54:00")));
 bookList.add(new Book("On the Road", "Novel", "Jack Kerouac"));
 bookList.add(new Book("The Myth of Sisyphus", "Essay", "Albert Camus"));
 
 console.log(bookList);
-console.log(bookList.NumberOfReadBooks);
+console.log(bookList.numberOfReadBooks);
+bookList.startReading();
+console.log(`Current book: ${bookList.currentBook}`);
+console.log(`Last book read: ${bookList.lastBookRead}`);
+bookList.finishCurrentBook();
+console.log(`Last book read: ${bookList.lastBookRead}`);
+bookList.startReading();
+console.log(`Current book: ${bookList.currentBook}`);
+console.log(`Last book read: ${bookList.lastBookRead}`);
+bookList.finishCurrentBook();
+console.log(`Last book read: ${bookList.lastBookRead}`);
+bookList.startReading();
+console.log(`Current book: ${bookList.currentBook}`);
+console.log(`Last book read: ${bookList.lastBookRead}`);
+bookList.finishCurrentBook();
+console.log(`Last book read: ${bookList.lastBookRead}`);
 bookList.startReading();
